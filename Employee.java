fb886abb464c111c919a2b61ece4c6e80d679a93
Anton Khomutinnikov <73312346+WBADM635@users.noreply.github.com>
2020-11-06 04:03:44 +0700

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,38 @@
+public class Employee {
+    String fullname;
+    String position;
+    String email;
+    String phone;
+    int salary;
+    int age;
+
+    public Employee(String fullname, String position, String email, String phone, int salary, int age) {
+        this.fullname = fullname;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    void info() {
+        System.out.println(String.format("ФИО        %s", fullname));
+        System.out.println(String.format("Должность  %s", position));
+        System.out.println(String.format("email      %s", email));
+        System.out.println(String.format("Телефон    %s", phone));
+        System.out.println(String.format("Зарплата   %s", salary));
+        System.out.println(String.format("Возраст    %s", age));
+        System.out.println();
+    }
+
+    void old() {
+        System.out.println("Старше 40 лет:");
+        System.out.println(String.format("ФИО        %s", fullname));
+        System.out.println(String.format("Должность  %s", position));
+        System.out.println(String.format("email      %s", email));
+        System.out.println(String.format("Телефон    %s", phone));
+        System.out.println(String.format("Зарплата   %s", salary));
+        System.out.println(String.format("Возраст    %s", age));
+        System.out.println();
+    }
+}
